5a10c86651664384dfbfd30d4653c24b12934ae5
melodyyunghsin <93466510+melodyyunghsin@users.noreply.github.com>
2025-01-15 15:30:51 +0800

    Add files via upload

---
+++ F74126343_practice_6_1/F74126343_practice_6_1/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace F74126343_practice_6_1
+{
+    public partial class Form1 : Form
+    {
+        int blockWidth = 50;
+        int blockHeight = 50;
+        int columns = 30;
+        int rows = 15;
+        Block.BlockType selectedType = Block.BlockType.DIRT;
+        Block[,] blocks = new Block[30, 15];
+        bool gameStart = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            gamePanel.Visible = false;
+            landscapePanel.MouseClick += landscapePanel_MouseClick;
+            vScrollBar1.Minimum = 0;
+            vScrollBar1.Maximum = landscapePanel.Height - viewPanel.Height;
+            vScrollBar1.SmallChange = blockHeight;
+            //vScrollBar1.LargeChange = blockHeight * rows - viewPanel.Height;
+
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Maximum = landscapePanel.Width - viewPanel.Width;
+            hScrollBar1.SmallChange = blockHeight;
+            //hScrollBar1.LargeChange = blockHeight * rows - viewPanel.Height;
+
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Vector2F position = new Vector2F(i, j);
+                    Block newBlock;
+                    if (j >= 10) newBlock = new Block(position, Block.BlockType.STONE);
+                    else if (j < 10 && j >= 8)
+                    {
+                        newBlock = new Block(position, Block.BlockType.DIRT);
+                    }
+                    else if (j == 7)
+                    {
+                        if (i / 5 == 2 || i / 5 == 4) newBlock = new Block(position, Block.BlockType.DIRT);
+                        else newBlock = new Block(position, Block.BlockType.GRASS_BLOCK);
+                    }
+                    else if (j == 6)
+                    {
+                        if (i / 5 == 2 || i / 5 == 4) newBlock = new Block(position, Block.BlockType.GRASS_BLOCK);
+                        else continue; 
+                    }
+                    else continue;
+                    blocks[i, j] = newBlock;
+                }
+            }
+        }
+
+        private void landscapePanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point blockLocation = new Point(e.X, e.Y);
+            if(sender.GetType() == typeof(PictureBox))
+            {
+                PictureBox pictureBox = (PictureBox)sender;
+                blockLocation = pictureBox.Location;
+            }
+            Vector2F selectedLocation = new Vector2F(blockLocation.X / blockWidth, blockLocation.Y / blockHeight);
+            //MessageBox.Show($"{selectedLocation.X}/{selectedLocation.Y}");
+            if (e.Button == MouseButtons.Right)
+            {
+                Block newBlock = new Block(selectedLocation, selectedType);
+                if (blocks[selectedLocation.X, selectedLocation.Y] == null)
+                {
+                    blocks[selectedLocation.X, selectedLocation.Y] = newBlock;
+                    placeBlock(newBlock);
+                }
+            }
+            else if(e.Button == MouseButtons.Left)
+            {
+                Block deleteBlock = blocks[selectedLocation.X, selectedLocation.Y];
+                if(deleteBlock != null)
+                {
+                    //MessageBox.Show("deleting");
+                    deleteBlock.Picture.Visible = false;
+                    deleteBlock.Picture.Dispose();
+                    landscapePanel.Controls.Remove(deleteBlock.Picture);
+                    blocks[selectedLocation.X, selectedLocation.Y] = null;
+                }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            gameStart = true;
+            startPanel.Visible = false;
+            gamePanel.Visible = true;
+            viewPanel.Visible = true;
+            landscapePanel.Visible = true;
+            vScrollBar1.Visible = true;
+            hScrollBar1.Visible = true;
+            viewPanel.AutoScroll = false;
+            landscapePanel.AutoScroll = false;
+            vScrollBar1.BringToFront();
+            hScrollBar1.BringToFront();
+
+            CreateTerritory();
+            steve.Location = new Point(0, blocks[0, 7].Position.Y * blockHeight - steve.Height);
+        }
+
+        private void CreateTerritory()
+        {
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    Block getBlock = blocks[i,j];
+                    if (getBlock == null) continue;
+                    placeBlock(getBlock);
+                }
+            }
+        }
+
+        private void placeBlock(Block block)
+        {
+            block.Picture.Location = new Point(block.Position.X * blockWidth, block.Position.Y * blockHeight);
+            landscapePanel.Controls.Add(block.Picture);
+            block.Picture.Visible = true;
+            block.Picture.MouseClick += landscapePanel_MouseClick;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameStart && e.KeyCode >= Keys.D1 && e.KeyCode <= Keys.D9)
+            {
+                if (e.KeyCode == Keys.D1) selectedType = Block.BlockType.DIRT;
+                else if (e.KeyCode == Keys.D2) selectedType = Block.BlockType.GRASS_BLOCK;
+                else if (e.KeyCode == Keys.D3) selectedType = Block.BlockType.STONE;
+                else if (e.KeyCode == Keys.D4) selectedType = Block.BlockType.WATER;
+                int selectedSlot = e.KeyCode - Keys.D1;
+                selectedFrame.Location = new Point(selectedSlot * 47, 0);
+            }
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int x = landscapePanel.Location.X;
+            int y = -vScrollBar1.Value;
+            landscapePanel.Location = new Point(x, y);   
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            int x = -hScrollBar1.Value;
+            int y = landscapePanel.Location.Y;
+            landscapePanel.Location = new Point(x, y);
+        }
+    }
+    public class Vector2F
+    {
+        int x, y;
+        public int X
+        {
+            get => x;
+            set => x = value;
+        }
+        public int Y
+        {
+            get => y;
+            set => y = value;
+        }
+        public Vector2F(int x_val, int y_val)
+        {
+            x = x_val;
+            y = y_val;
+        }
+    }
+    public class Block
+    {
+        public readonly Vector2F Position;
+        public readonly BlockType Type;
+        public PictureBox Picture;
+
+        public enum BlockType
+        {
+            WATER,
+            GRASS_BLOCK,
+            DIRT,
+            STONE,
+        }
+
+        public Block(Vector2F position, BlockType type)
+        {
+            Position = position;
+            Type = type;
+            Picture = new PictureBox();
+            Picture.Size = new Size(50, 50);
+            Picture.SizeMode = PictureBoxSizeMode.Zoom;
+            Picture.Padding = new Padding(0);
+            Picture.Margin = new Padding(0);
+            if (Type == BlockType.WATER) Picture.Image = Properties.Resources.Water;
+            else if(Type == BlockType.GRASS_BLOCK) Picture.Image = Properties.Resources.GrassDirt;
+            else if(Type == BlockType.DIRT) Picture.Image = Properties.Resources.Dirt;
+            else if(Type == BlockType.STONE) Picture.Image = Properties.Resources.Stone;
+        }
+    }
+
+}
---
+++ F74126343_practice_6_2/F74126343_practice_6_2/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace F74126343_practice_6_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startPanel = new System.Windows.Forms.Panel();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.viewPanel = new System.Windows.Forms.Panel();
+            this.InventoryBar = new System.Windows.Forms.Panel();
+            this.inventoryWater = new System.Windows.Forms.PictureBox();
+            this.inventoryGrass = new System.Windows.Forms.PictureBox();
+            this.inventoryStone = new System.Windows.Forms.PictureBox();
+            this.inventoryDirt = new System.Windows.Forms.PictureBox();
+            this.selectedFrame = new System.Windows.Forms.PictureBox();
+            this.landscapePanel = new System.Windows.Forms.Panel();
+            this.steve = new System.Windows.Forms.PictureBox();
+            this.pausePanel = new System.Windows.Forms.Panel();
+            this.backButton = new System.Windows.Forms.Button();
+            this.saveFileButton = new System.Windows.Forms.Button();
+            this.resumeButton = new System.Windows.Forms.Button();
+            this.startPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gamePanel.SuspendLayout();
+            this.viewPanel.SuspendLayout();
+            this.InventoryBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryWater)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGrass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryStone)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryDirt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedFrame)).BeginInit();
+            this.landscapePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.steve)).BeginInit();
+            this.pausePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startPanel
+            // 
+            this.startPanel.AutoSize = true;
+            this.startPanel.Controls.Add(this.exitButton);
+            this.startPanel.Controls.Add(this.openFileButton);
+            this.startPanel.Controls.Add(this.startButton);
+            this.startPanel.Controls.Add(this.pictureBox2);
+            this.startPanel.Controls.Add(this.pictureBox1);
+            this.startPanel.Location = new System.Drawing.Point(1, 0);
+            this.startPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.startPanel.Name = "startPanel";
+            this.startPanel.Size = new System.Drawing.Size(702, 453);
+            this.startPanel.TabIndex = 0;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(249, 319);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(200, 36);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += exitButton_Click;
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openFileButton.Location = new System.Drawing.Point(249, 259);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(200, 36);
+            this.openFileButton.TabIndex = 5;
+            this.openFileButton.Text = "Open File";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(249, 200);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(200, 36);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = global::F74126343_practice_6_2.Properties.Resources.MinecraftLogo;
+            this.pictureBox2.Location = new System.Drawing.Point(196, 124);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(311, 52);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::F74126343_practice_6_2.Properties.Resources.startPage;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(699, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.Controls.Add(this.hScrollBar1);
+            this.gamePanel.Controls.Add(this.vScrollBar1);
+            this.gamePanel.Controls.Add(this.viewPanel);
+            this.gamePanel.Location = new System.Drawing.Point(0, 0);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(698, 450);
+            this.gamePanel.TabIndex = 2;
+            this.gamePanel.Visible = false;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(1, 433);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(680, 17);
+            this.hScrollBar1.TabIndex = 9;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(681, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 427);
+            this.vScrollBar1.TabIndex = 8;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // viewPanel
+            // 
+            this.viewPanel.Controls.Add(this.InventoryBar);
+            this.viewPanel.Controls.Add(this.landscapePanel);
+            this.viewPanel.Location = new System.Drawing.Point(0, 0);
+            this.viewPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.viewPanel.Name = "viewPanel";
+            this.viewPanel.Size = new System.Drawing.Size(681, 433);
+            this.viewPanel.TabIndex = 7;
+            this.viewPanel.Visible = false;
+            // 
+            // InventoryBar
+            // 
+            this.InventoryBar.BackgroundImage = global::F74126343_practice_6_2.Properties.Resources.ToolBar;
+            this.InventoryBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.InventoryBar.Controls.Add(this.inventoryWater);
+            this.InventoryBar.Controls.Add(this.inventoryGrass);
+            this.InventoryBar.Controls.Add(this.inventoryStone);
+            this.InventoryBar.Controls.Add(this.inventoryDirt);
+            this.InventoryBar.Controls.Add(this.selectedFrame);
+            this.InventoryBar.Location = new System.Drawing.Point(130, 371);
+            this.InventoryBar.Name = "InventoryBar";
+            this.InventoryBar.Size = new System.Drawing.Size(430, 54);
+            this.InventoryBar.TabIndex = 1;
+            // 
+            // inventoryWater
+            // 
+            this.inventoryWater.Image = global::F74126343_practice_6_2.Properties.Resources.Water;
+            this.inventoryWater.Location = new System.Drawing.Point(155, 14);
+            this.inventoryWater.Name = "inventoryWater";
+            this.inventoryWater.Size = new System.Drawing.Size(25, 25);
+            this.inventoryWater.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inventoryWater.TabIndex = 3;
+            this.inventoryWater.TabStop = false;
+            // 
+            // inventoryGrass
+            // 
+            this.inventoryGrass.Image = global::F74126343_practice_6_2.Properties.Resources.GrassDirt;
+            this.inventoryGrass.Location = new System.Drawing.Point(60, 14);
+            this.inventoryGrass.Name = "inventoryGrass";
+            this.inventoryGrass.Size = new System.Drawing.Size(25, 25);
+            this.inventoryGrass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inventoryGrass.TabIndex = 2;
+            this.inventoryGrass.TabStop = false;
+            // 
+            // inventoryStone
+            // 
+            this.inventoryStone.Image = global::F74126343_practice_6_2.Properties.Resources.Stone;
+            this.inventoryStone.Location = new System.Drawing.Point(108, 14);
+            this.inventoryStone.Name = "inventoryStone";
+            this.inventoryStone.Size = new System.Drawing.Size(25, 25);
+            this.inventoryStone.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inventoryStone.TabIndex = 1;
+            this.inventoryStone.TabStop = false;
+            // 
+            // inventoryDirt
+            // 
+            this.inventoryDirt.Image = global::F74126343_practice_6_2.Properties.Resources.Dirt;
+            this.inventoryDirt.Location = new System.Drawing.Point(13, 14);
+            this.inventoryDirt.Name = "inventoryDirt";
+            this.inventoryDirt.Size = new System.Drawing.Size(25, 25);
+            this.inventoryDirt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inventoryDirt.TabIndex = 0;
+            this.inventoryDirt.TabStop = false;
+            // 
+            // selectedFrame
+            // 
+            this.selectedFrame.BackColor = System.Drawing.Color.Transparent;
+            this.selectedFrame.Image = global::F74126343_practice_6_2.Properties.Resources.SelectedFrame;
+            this.selectedFrame.Location = new System.Drawing.Point(0, 0);
+            this.selectedFrame.Name = "selectedFrame";
+            this.selectedFrame.Size = new System.Drawing.Size(54, 53);
+            this.selectedFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.selectedFrame.TabIndex = 4;
+            this.selectedFrame.TabStop = false;
+            // 
+            // landscapePanel
+            // 
+            this.landscapePanel.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.landscapePanel.Controls.Add(this.steve);
+            this.landscapePanel.Location = new System.Drawing.Point(0, 0);
+            this.landscapePanel.Margin = new System.Windows.Forms.Padding(0);
+            this.landscapePanel.Name = "landscapePanel";
+            this.landscapePanel.Size = new System.Drawing.Size(1500, 750);
+            this.landscapePanel.TabIndex = 0;
+            // 
+            // steve
+            // 
+            this.steve.Image = global::F74126343_practice_6_2.Properties.Resources.Steve;
+            this.steve.Location = new System.Drawing.Point(0, 300);
+            this.steve.Name = "steve";
+            this.steve.Size = new System.Drawing.Size(50, 95);
+            this.steve.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.steve.TabIndex = 0;
+            this.steve.TabStop = false;
+            // 
+            // pausePanel
+            // 
+            this.pausePanel.BackColor = System.Drawing.Color.DarkGray;
+            this.pausePanel.Controls.Add(this.backButton);
+            this.pausePanel.Controls.Add(this.saveFileButton);
+            this.pausePanel.Controls.Add(this.resumeButton);
+            this.pausePanel.Location = new System.Drawing.Point(1, 0);
+            this.pausePanel.Name = "pausePanel";
+            this.pausePanel.Size = new System.Drawing.Size(698, 450);
+            this.pausePanel.TabIndex = 5;
+            this.pausePanel.Visible = false;
+            // 
+            // backButton
+            // 
+            this.backButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backButton.Location = new System.Drawing.Point(234, 259);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(230, 45);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "Back to Home Page";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // saveFileButton
+            // 
+            this.saveFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveFileButton.Location = new System.Drawing.Point(234, 203);
+            this.saveFileButton.Name = "saveFileButton";
+            this.saveFileButton.Size = new System.Drawing.Size(230, 45);
+            this.saveFileButton.TabIndex = 1;
+            this.saveFileButton.Text = "Save File";
+            this.saveFileButton.UseVisualStyleBackColor = true;
+            this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // resumeButton
+            // 
+            this.resumeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resumeButton.Location = new System.Drawing.Point(235, 150);
+            this.resumeButton.Name = "resumeButton";
+            this.resumeButton.Size = new System.Drawing.Size(230, 45);
+            this.resumeButton.TabIndex = 0;
+            this.resumeButton.Text = "Resume";
+            this.resumeButton.UseVisualStyleBackColor = true;
+            this.resumeButton.Click += new System.EventHandler(this.resumeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.Controls.Add(this.pausePanel);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.startPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.startPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gamePanel.ResumeLayout(false);
+            this.viewPanel.ResumeLayout(false);
+            this.InventoryBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryWater)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGrass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryStone)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryDirt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedFrame)).EndInit();
+            this.landscapePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.steve)).EndInit();
+            this.pausePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel startPanel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Panel viewPanel;
+        private System.Windows.Forms.Panel InventoryBar;
+        private System.Windows.Forms.PictureBox inventoryWater;
+        private System.Windows.Forms.PictureBox inventoryGrass;
+        private System.Windows.Forms.PictureBox inventoryStone;
+        private System.Windows.Forms.PictureBox inventoryDirt;
+        private System.Windows.Forms.PictureBox selectedFrame;
+        private System.Windows.Forms.Panel landscapePanel;
+        private System.Windows.Forms.PictureBox steve;
+        private System.Windows.Forms.Panel pausePanel;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.Button resumeButton;
+    }
+}
+
